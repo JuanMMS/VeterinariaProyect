e5e192e7b3c001612312dd6f79ce31fcc213b1bc
JuanMMS <juanm.mosqueras@uqvirtual.edu.co>
2025-08-11 16:50:42 -0500

    Update

---
+++ src/main/java/co/edu/uniquindio/poo/veterinariaproyect/model/ClinicaVeterinaria.java
@@ -0,0 +1,167 @@
+package co.edu.uniquindio.poo.veterinariaproyect.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClinicaVeterinaria {
+
+    private String nombre;
+    private String ID;
+    private List<Persona> listPersonas;
+    private List<Mascota> listMascotas;
+    private List<Consulta> listConsultas;
+    private List<Cita> listCitas;
+
+
+    public ClinicaVeterinaria(String nombre, String ID) {
+        this.nombre = nombre;
+        this.ID = ID;
+        this.listPersonas = new ArrayList<>();
+        this.listMascotas= new ArrayList<>();
+        this.listConsultas= new ArrayList<>();
+        this.listCitas= new ArrayList<>();
+
+    }
+
+
+    /*
+    Metodo para agregar Personas desde la clase ClinicaVeterinaria
+     */
+    public void agregarPersona(Persona persona) {
+        listPersonas.add(persona);
+    }
+
+    /*
+    Metodo para agregar Mascotas desde la clase ClinicaVeterinaria
+     */
+    public void agregarMascota(Mascota mascota) {
+        listMascotas.add(mascota);
+    }
+
+    /*
+    Metodo para agregar Consultas desde la clase ClinicaVeterina
+     */
+    public void agregarConsulta(Consulta consulta) {
+        listConsultas.add(consulta);
+    }
+
+    /*
+    Metodo para agregar Citas desde la clase ClinicaVeterinaria
+    */
+    public void agregarCita(Cita cita) {
+        listCitas.add(cita);
+    }
+
+    /*
+    Metodos getter y setter de la clase ClinicaVeterinaria
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /*
+    Metodos getter y setter de la clase ClinicaVeterinaria
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /*
+    Metodos getter y setter de la clase ClinicaVeterinaria
+     */
+    public String getID() {
+        return ID;
+    }
+
+    /*
+    Metodos getter y setter de la clase ClinicaVeterinaria
+     */
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    /*
+    Metodos getter y setter de la clase ClinicaVeterinaria
+     */
+    public List<Persona> getListPersonas() {
+        return listPersonas;
+    }
+
+    /*
+    Metodos getter y setter de la clase ClinicaVeterinaria
+     */
+    public void setListPersonas(List<Persona> listPersonas) {
+        this.listPersonas = listPersonas;
+    }
+
+    /*
+    Metodos getter y setter de la clase ClinicaVeterinaria
+     */
+    public List<Mascota> getListMascotas() {
+        return listMascotas;
+    }
+
+    /*
+    Metodos getter y setter de la clase ClinicaVeterinaria
+     */
+    public void setListMascotas(List<Mascota> listMascotas) {
+        this.listMascotas = listMascotas;
+    }
+
+    /*Metodos getter y setter de la clase ClinicaVeterinaria
+
+     */
+    public List<Consulta> getListConsultas() {
+        return listConsultas;
+    }
+
+    /*
+    Metodos getter y setter de la clase ClinicaVeterinaria
+     */
+    public void setListConsultas(List<Consulta> listConsultas) {
+        this.listConsultas = listConsultas;
+    }
+
+    /*
+    Metodos getter y setter de la clase CliniicaVeterinaria
+     */
+    public List<Cita> getListCitas() {
+        return listCitas;
+    }
+
+    /*
+    Metodos getter y setter de la clase ClinicaVeterinaria
+     */
+    public void setListCitas(List<Cita> listCitas) {
+        this.listCitas = listCitas;
+    }
+
+    /*
+    Metodo eliminar persona desde la clase ClinicaVeterinaria
+     */
+    public void eliminarPersona(Persona persona) {
+        listPersonas.remove(persona);
+    }
+
+    /*
+     Metodo eliminar una mascota desde la clase ClinicaVeterinaria
+     */
+    public void eliminarMascota(Mascota mascota) {
+        listMascotas.remove(mascota);
+    }
+
+    /*
+    Metodo eliminar una consultas desde la clase ClinicaVeterinaria
+     */
+    public void eliminarConsultas(Consulta consulta) {
+        listConsultas.remove(consulta);
+    }
+
+    /*
+    Metodo eliminar citas desde la clase ClinicaVeterinaria
+     */
+    public void eliminarCitas(Cita cita) {
+        listCitas.remove(cita);
+    }
+
+}
