3e1ef275d039bd6045c0bbfffa7f4e92a50517a9
Juan José Ortiz Loaiza <j22ortizl@gmail.com>
2025-08-11 20:08:29 -0500

    JAVAFX

---
+++ src/main/java/co/edu/uniquindio/poo/veterinariaproyect/controller/MenuController.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.poo.veterinariaproyect.controller;
+
+public class MenuController {
+}
---
+++ src/main/java/co/edu/uniquindio/poo/veterinariaproyect/viewController/RegistrosViewController.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.poo.veterinariaproyect.viewController;
+
+public class RegistrosViewController {
+}
